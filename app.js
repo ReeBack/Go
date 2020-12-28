951ed035e736a9d87ffd743ea9da73395a4f7aa4
Reback <54481070+ReeBack@users.noreply.github.com>
2020-12-27 19:46:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-var car = "Go";
+var games = "ty";
 
+console.log();
+console.log(games);
+
+var life = 100;
+var name = "Dev Ed";
+
+console.log(life);
+
+life = 50;
+
+console.log(life);
+
+life = life - 10;
+
+console.log(life);
+
+var checkout = true;
+
+if (checkout) {
+    console.log(life);
+}
+
+var live = 100;
+console.log("Live " + live);
+
+document.getElementById = "hi";
+
+const car = "goo";
 console.log(car);
+
+console.log();
