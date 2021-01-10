c233b9789b7e435f2744cbc9a011d36e256d40fe
Reback <54481070+ReeBack@users.noreply.github.com>
2021-01-09 21:15:02 -0500

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -12,3 +12,21 @@ document.write("hy hi hehehehe");
 
 var live = 1;
 console.log(live);
+
+var isDog = true;
+var isCat = false;
+
+console.log(isDog + isCat);
+
+var isApp = true;
+var isAp = false;
+
+var isOk = true;
+var isNo = false;
+
+console.log(isOk);
+document.write("\n" + isNo);
+
+// JavaScript
+
+alert("ok I am a good")
